7ddc7b855ef9aced703c87d41bb5e6ddcc43ee3b
AntonKharitonovgl <55668728+AntonKharitonovgl@users.noreply.github.com>
2020-03-07 19:19:48 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(".example").vegas({
     slides: [
-        { src: "images/slaider1.jpg" },
-        { src: "images/logo3.jpg" }
+        { src: "/images/slaider1.jpg" },
+        { src: "/images/logo3.jpg" }
     ]
 });
