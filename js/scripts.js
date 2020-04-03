3a72f449c7f7ae12c398264ecc62325876d3c1a7
AntonKharitonovgl <55668728+AntonKharitonovgl@users.noreply.github.com>
2020-04-03 17:02:13 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,13 @@ $(document).ready(function(){
       $('.slider').bxSlider({
   pager: false,
    captions: true,
+   speed: 1000,
+   controls: false,
    nextText: '',
    prevText: '',
    easing: 'jswing',
+   auto: true,
+   pause: 10000
    });
     });
 
