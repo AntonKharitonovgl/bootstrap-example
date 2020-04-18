4fd084167cbedf98e39a27960f99ec4b08b45129
AntonKharitonovgl <55668728+AntonKharitonovgl@users.noreply.github.com>
2020-04-18 22:03:25 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,37 @@ $(document).ready(function(){
    });
     });
 
+$(document).ready(function(){
+    $('.bxslider-max').bxSlider(
+{
+  pager: false,
+   captions: true,
+   speed: 1000,
+   controls: false,
+   nextText: '',
+   prevText: '',
+   easing: 'jswing',
+   auto: true,
+   pause: 10000
+   }
+    	);
+    
+  });
+
+$(document).ready(function(){
+    
+    $('.bxslider-min').bxSlider(
+{
+  pager: false,
+   captions: true,
+   speed: 1000,
+   controls: false,
+   nextText: '',
+   prevText: '',
+   easing: 'jswing',
+   auto: true,
+   pause: 10000
+   }
+
+    	);
+  });
